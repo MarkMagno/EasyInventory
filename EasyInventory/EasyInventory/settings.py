6859f054d0b06890eb23a03bb222c80b6f6b5a63
MarkMagno <44620752+MarkMagno@users.noreply.github.com>
2024-12-16 18:26:02 -0600

    Update settings.py

--- EasyInventory/EasyInventory/settings.py
+++ EasyInventory/EasyInventory/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR / "inventory" / "static"]
+##STATICFILES_DIRS = [BASE_DIR / "inventory" / "static"]
 
 LOGIN_URL = '/login/'  # Adjust this path as necessary
 LOGIN_REDIRECT_URL = '/'  # Adjust this for post-login redirection
